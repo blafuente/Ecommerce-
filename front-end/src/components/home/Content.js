2f91ce73d5d1d90e376c2390fc7ca1129823ef94
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-02-28 14:24:23 -0500

    data from game api

--- front-end/src/components/home/Content.js
+++ front-end/src/components/home/Content.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {Component } from 'react';
 import GameCard from '../utility/GameCard';
+import axios from 'axios';
 
-function Content(props){
-    return(
-        <div className="row">
-            <div className="col s12">
-                <GameCard />
-                <GameCard />
-                <GameCard />
-                <GameCard />
+class Content extends Component{
+    constructor(){
+        super();
+        this.state={
+            games:[]
+        }
+    }
+    componentDidMount(){
+        const gamesPromise = axios.get(`${window.apiHost}/games/getHome`);
+        gamesPromise.then((response)=>{
+            const games = response.data;
+            this.setState({
+                games: games
+            })
+        });
+    }
+    render(){
+        const gameCards = this.state.games.map((game,i)=>{
+            return (<GameCard key={i} data={game} />)
+        })
+        return(
+            <div className="row">
+                <div className="col s12">
+                    {gameCards}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+
+
 
 export default Content
